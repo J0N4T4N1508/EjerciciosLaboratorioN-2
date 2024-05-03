ccd7f6144b6f9c4e6f84a9a4b836861f7e2feebd
J0N4T4N1508 <167550570+J0N4T4N1508@users.noreply.github.com>
2024-05-03 15:50:46 -0500

    Add files via upload

---
+++ EJERCICIO-10/Program.cs
@@ -0,0 +1,26 @@
+﻿// 10) Genera los primeros 10 números de la serie Fibonacci. 
+
+Console.WriteLine("Números de la Serie Fibonacci");
+Console.WriteLine("");
+
+int n = 10; // Número de términos de la serie Fibonacci a generar
+
+// Inicialización de los primeros dos términos de la serie Fibonacci
+int a = 0;
+int b = 1;
+
+Console.WriteLine("Los primeros " + n + " números de la serie Fibonacci son:");
+
+// Itera para generar y mostrar los primeros 'n' términos de la serie Fibonacci.
+for (int i = 0; i < n; i++)
+{
+    Console.Write(a + " ");
+
+    // Actualiza 'a' y 'b' para generar el siguiente término de la serie Fibonacci.
+    int temp = a;
+    a = b;
+    b = temp + b;
+}
+
+Console.ReadKey();
+
